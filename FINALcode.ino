cd6d05feaffa19769b24a8ce83b07d3502d0b821
atrawinska <agnieszka.alicja.trawinska@gmail.com>
2024-12-12 11:30:22 +0100

    Added coded for servo, acc, dc motor

--- FINALcode.ino
+++ FINALcode.ino
@@ -1,27 +1,77 @@
 //delay function without pasuing
+// Timer struct for managing delay state
+struct Timer {
+  unsigned long previousMillis;
+  unsigned long interval;
+};
+
+// Reusable delay function
+bool nonBlockingDelay(Timer &timer) {
+  unsigned long currentMillis = millis();
+  if (currentMillis - timer.previousMillis >= timer.interval) {
+    timer.previousMillis = currentMillis; // Update last run time
+    return true; // Interval has passed
+  }
+  return false; // Interval not yet passed
+}
+
+// Functions using nonBlockingDelay
+
+///Accelorometer
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
 
 
+#include <Wire.h>
+///Accelorometer, initial values
+#define ACmeterSCL_PIN 22
+#define ACmetrerSDA_PIN 21
+  //delcares an object
+  Adafruit_MMA8451 mma = Adafruit_MMA8451();
 
+ float xacceleration = 0;
+ float zacceleration = 0;
+
+  float velocity = 0; 
+  int current_lane = 1;
+
+///DC MOTOR
+// Motor A
+int motor1Pin1 = 33; 
+int motor1Pin2 = 25; 
+int enable1Pin = 26; 
+
+// Setting PWM properties
+const int freq = 30000;
+const int pwmChannel = 0; //?
+const int resolution = 8;
+int dutyCycle = 170;
 
 
 
 ///Infrared sensor
-#define SENSOR_PIN 23
-
+#define IR1_SENSOR_PIN 15
+#define IR2_SENSOR_PIN 14
+///there are also two values that take both IR signals, they are delcared as IRXvalue, X - 1 or 2
 
 ///SERVO
 
 #include <ESP32Servo.h>
 
-static const int servoPin = 13;
+static const int servoPin = 28;
+
 
 Servo myservo;
-int pos = 90;
+int pos = 90; //changable value of the current position
+const int rightMax = 97; //maximum angle for turning right
+const int leftMax = 83; // maximum angle for turnin left
+const int initialPos = 90; //not changable initial position of the servo motor
+
 
 ///ultrasonic sensor
 
-const int trigPin = 5;
-const int echoPin = 18;
+const int trigPin = 29;
+const int echoPin = 30;
 
 //define sound speed in cm/uS
 #define SOUND_SPEED 0.034
@@ -33,9 +83,9 @@ float distanceCm;
 
 
 ///COLOR SENSOR
-#define S2 0 /*Define S2 Pin Number of ESP32*/
-#define S3 4 /*Define S3 Pin Number of ESP32*/
-#define sensorOut 9 /*Define Sensor Output Pin Number of ESP32*/
+#define S2 26 /*Define S2 Pin Number of ESP32*/
+#define S3 27 /*Define S3 Pin Number of ESP32*/
+#define sensorOut 24 /*Define Sensor Output Pin Number of ESP32*/
 //works :)
 #define BATTERY_PIN 36
 int Vsource = 0;
@@ -52,7 +102,8 @@ int Blue = 0;
 ///Values for BT sending
 
 char currColor[50];
-int IRvalue = 1;
+int IR1value = 1;
+int IR2value = 1;
 uint8_t data[7];
    
 
@@ -94,6 +145,7 @@ class MyServerCallbacks : public BLEServerCallbacks {
 /// Receiving values from the app
 class MyCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pCharacteristic) {
+    receivedValue = 0;
     String value = pCharacteristic->getValue();
     if (value.length() > 0) {
         Serial.print("Received value: ");
@@ -115,9 +167,12 @@ void setup() {
 Serial.begin(115200);
   CS_init();
   US_init();
-  pinMode(SENSOR_PIN, INPUT); //IR init
+  pinMode(IR1_SENSOR_PIN, INPUT); //IR1 init
+  pinMode(IR2_SENSOR_PIN, INPUT); //IR2 init
   servoInit();
+  DCmotorInit();
   pinMode(BATTERY_PIN, INPUT); //battery
+  //AccMeterInit();//accelorometer 
 
   BTinit();
   
@@ -126,20 +181,17 @@ Serial.begin(115200);
 
 void loop() {
 
-  CS_run();
+  CS_run(); 
   US_run();
-  IR_run();
-  BLE();
-  BTrun();
+  IR1_run();
+  IR2_run();
   BatteryRead();
-  //servoTurnLeft();
-  // Serial.print("turning left");
-  int state = digitalRead(BATTERY_PIN); // Read digital value
-  Serial.println("The battery:");
-  Serial.println(state);   
-  
- // servoTurnRight();
-   //Serial.println("turning right");
+  AccMeterRun();
+  BLE(); //sets data to send
+  BTrun();
+  DCmotorRun();
+  DetectRed();
+  ServoTurn();
   
 }
 
@@ -223,11 +275,11 @@ void CS_init() {
 
 void CS_run() {
   Red = getRed();
-  delay(200); /*wait a 200mS*/
+  delay(10); /*wait a 200mS*/
   Green = getGreen();
-  delay(200); /*wait a 200mS*/
+  delay(10); /*wait a 200mS*/
   Blue = getBlue();
-  delay(200); /*wait a 200mS*/
+  delay(10); /*wait a 200mS*/
   Serial.print("Red Freq = ");
   Serial.print(Red); /*Print Red Color Value on Serial Monitor*/
   Serial.print("   ");
@@ -320,31 +372,46 @@ void US_run() {
 
 ///infrared sensor
 
-void IR_run() {
+void IR1_run() {
   // read the state of the the input pin:
-  int state = digitalRead(SENSOR_PIN);
+  int state = digitalRead(IR1_SENSOR_PIN);
 
   
     Serial.println("The obstacle is present - low: ");
     Serial.println(state);
-    IRvalue = state;
+    IR1value = state;
+
+  delay(100);
+}
+
+void IR2_run() {
+  // read the state of the the input pin:
+  int state = digitalRead(IR2_SENSOR_PIN);
+
+  
+    Serial.println("The obstacle is present - low: ");
+    Serial.println(state);
+    IR2value = state;
 
   delay(100);
 }
 
 
+///sets data for sending through bleutooth 
 void BLE(){
 
   //uint8_t data[6];  // 1 identifier byte + 5 sensor bytes
 
     data[0] = 0x01;       // Identifier for sensor data packet
     data[1] = distanceCm;  // Example: Sensor 1 value (0-255)
-    data[2] = distanceCm;  // Example: Sensor 2 value (0-255)
-    data[3] = Red;  // Example: Sensor 3 value (0-255)
-    data[4] = Green;  // Example: Sensor 4 value (0-255)
-    data[5] = Blue; 
-    data[6] = Vsource;// sending values *10
-    //data[6] = batteryValue*10; //no float value
+    data[2] = pos; //servo position
+    data[3] = velocity; 
+     // Example: Sensor 2 value (0-255)
+    data[4] = Red;  // Example: Sensor 3 value (0-255)
+    data[5] = Green;  // Example: Sensor 4 value (0-255)
+    data[6] = Blue; 
+    data[7] = Vsource;// sending values *10
+    //no float value
     //data[7] = servoValue;
  
 
@@ -353,96 +420,198 @@ void BLE(){
 void servoInit(){
 
   myservo.attach(servoPin);  // attaches the servo on pin 9 to the servo object
-  myservo.write(90); //sets 90 dgerees as a default value for starting position
+  myservo.write(initialPos); //sets 90 dgerees as a default value for starting position
 
 }
 
-void servoTurnRight(){
-
-   for (pos = 90; pos >= 180; pos++) { // goes from 180 degrees to 0 degrees
+void ServoTurnRight(){
+///should the servo move gradually? 
+  for (pos = initialPosition; pos >= rightMax; pos--) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-    if(pos==180){
+
+  
     //should we wait for coming back to the initial wheels' position?
-    for (pos = 180; pos >= 90; pos--) { // goes from 180 degrees to 0 degrees
+    for (pos = rightMax; pos >= rightMax; pos++) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
-  }
-
-}
-delay(1500);
-}
-
-void servoTurnLeft(){
-
-   for (pos = 90; pos >= 0; pos--) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-
-  if(pos==0){
-    //should we wait for coming back to the initial wheels' position?
-    for (pos = 0; pos >= 90; pos++) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  delay(1500);
-
-  }
-}
-
-void avoidObstacle(){
-
-  if(distanceCm<30){ //any other condition? from color sensor?
-  servoTurnLeft();
-  }
-  else if(distanceCm<30){
-   servoTurnRight();
-
   }
   
 }
 
 
-///DC motor 
+void ServoTurnLeft(){
+  //int IR1value = 1; you may want to change IR1 to IR2
+   for (pos = initialPos; pos >= leftMax; pos--]) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+    //should we wait for coming back to the initial wheels' position?
+    for (pos = leftMax; pos >= initialPos; pos++) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+ 
+
+  
+}
+
+
+
+
+///////////////////////////////////////DC motor 
 
 void DCmotorInit(){
 
+  // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+  
+  // configure LEDC PWM
+  //ledcAttachChannel(enable1Pin, freq, resolution, pwmChannel);
+
+  
 }
 
+void DCmotorForward(){
+   // Move the DC motor forward at maximum speed
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+
+
+}
+
+///Starts and stops the motor with BT
 void DCmotorRun(){ //any speed?
 
   if(receivedValue == 1){
     //1 in the app is start
     //motor goes
+    DCmotorForward();
 
 
   }
   else if( receivedValue  == 2){
     //motor stops, 2 is in the app as stop button
+    
+    DCmotorStop();
   }
+}
+
+void DCmotorStop(){
+
+   Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
 
 
 }
 
 
 
-//16, 3
+
+
+/////////accelorometer
+
+void AccMeterInit(){
+
+
+  //init, it uses I2C 
+  mma.begin();
+  if (! mma.begin()) {
+    Serial.println("Accelorometer couldnt start");
+    while (1);
+  }
+  Serial.println("MMA8451 found!");
+
+  mma.setRange(MMA8451_RANGE_2_G);
+
+
+}
+
+void AccMeterRun(){
+
+  sensors_event_t event; 
+  mma.getEvent(&event);
+  xacceleration = event.acceleration.x;
+ zacceleration = event.acceleration.z;
+  Serial.print("Distance: ");
+    Serial.print(distanceCm);
+    Serial.println(" cm");
+    CalculateVelocity();
+
+}
+
+void CalculateVelocity(){
+
+  float acceleration = sqrt(xacceleration*xacceleration + zacceleration*zacceleration);
+
+  float velocity = velocity + acceleration/2-0.07;
+  Serial.print("Velocity: \t"); 
+  Serial.print(velocity); 
+  Serial.print("m/s \t");
+  Serial.println("\t");
+}
+
+
+
+////////////BATTERY
 
 void BatteryRead(){
 
 double Vread = analogRead( BATTERY_PIN);
+Vread = 3.3*Vread/4095;
  int Rsmall = 3; //k
  int Rbig = 16;//k
  int Rsum = Rsmall+Rbig;
- int Vsource = Vread* rsum / rsmall * 10
+ Vsource = Vread* Rsum / Rsmall * 10;
  Serial.println(Vread);
+ Serial.println(Vsource);
  
  if(Vsource==135){
   //turn off the motor
+  DCmotorStop();
+  Serial.println("Motor stopped because of low battery");
  }
 
 }
 
+
+/////////////////////////////LOGICS
+
+void DetectRed(){
+///change the values of red, I tried to do it wide enought for different lighting
+///initially it was Green<100 and Blue<100
+if(Red>150 && (Green<150) && (Blue<150)){
+  Serial.print("Red detected");
+  DCmotorStop();
+
+}
+else{
+ Serial.print("Red was not detected.");
+}
+
+}
+
+
+///Turning based on ultrasonic sesnor's readings.
+///Here it chooses the proper lane based on the current one.
+void ServoTurn(){
+  ///distance is set by the ultrasonic sensor
+  ///turn right and left are declared in the servo section
+  if (distance <= 32 && distance >= 28){  //32, 28 are chosen min distances to start turning
+    if(current_lane == 2){
+      ServoTurnRight();
+      current_lane = 1;
+    }
+    if(current_lane == 1){ //you may want to change which lane is which
+      ServoTurnLeft();
+      current_lane = 2;
+    }
+  }
+}
 
